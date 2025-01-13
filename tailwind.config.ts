b2cd227c3f12c5d9a639ad407ec1b41fc09601fc
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-13 12:28:30 +0100

    style: add shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,93 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			keyframes: {
+                letter: {
+                    '0%': { transform: 'translate(0, 0) rotate(0deg)' },
+                    '10%': { transform: 'translate(1px, -1px) rotate(1deg)' },
+                    '20%': { transform: 'translate(-1px, 1px) rotate(-1deg)' },
+                    '30%': { transform: 'translate(1px, 1px) rotate(1deg)' },
+                    '40%': { transform: 'translate(-1px, -1px) rotate(-1deg)' },
+                    '50%': { transform: 'translate(1px, -1px) rotate(1deg)' },
+                    '60%': { transform: 'translate(-1px, 1px) rotate(-1deg)' },
+                    '70%': { transform: 'translate(1px, 1px) rotate(1deg)' },
+                    '80%': { transform: 'translate(-1px, -1px) rotate(-1deg)' },
+                    '90%': { transform: 'translate(1px, -1px) rotate(1deg)' },
+                    '100%': { transform: 'translate(0, 0) rotate(0deg)' }
+                },
+                sphere: {
+                    '0%': { transform: 'translate(0, 0) rotate(0deg)' },
+                    '25%': { transform: 'translate(10px, -10px) rotate(1deg)' },
+                    '50%': { transform: 'translate(-10px, 20px) rotate(-1deg)' },
+                    '75%': { transform: 'translate(-20px, -10px) rotate(1deg)' },
+                    '100%': { transform: 'translate(0, 0) rotate(0deg)' }
+                },
+            },
+            animation: {
+                letter: 'letter 5s ease-in-out infinite',
+                sphere: 'sphere 5s ease-in-out infinite'
+            }
+		},
+		fontFamily: {
+			'bloop': ['"Dx Bloop"', 'sans-serif'],
+			'mono': ['"JetBrains Mono"', 'monospace']
+
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
