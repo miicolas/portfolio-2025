aa29ccc7e0ba6efd957be3b75b9b0eabead2ed7f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-15 08:13:40 +0100

    ui: bg hover animation disable

--- app/projects/_projects/card-project.tsx
+++ app/projects/_projects/card-project.tsx
@@ -1,38 +1,37 @@
 'use client'
 import  Card  from "@/components/ui/card"
 import Image from "next/image"
-import { MoveDiagonal } from "lucide-react"
+import {MoveDiagonal } from "lucide-react"
 import { useState } from "react"
+import Link from "next/link"
 
 const title = "Project title"
 const description = "Project description"
 
 export default function CardProject() {
-    const [isHovered, setIsHovered] = useState(false)
 
     return (
-        <Card className="w-[70vw] h-[70vh] bg-neutral-100 rounded-lg p-10 overflow-hidden">
-            <div className="w-full h-full flex justify-between" onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}>
+        <Card className="w-[70vw] h-[70vh] xl:w-[100vh] bg-neutral-100 rounded-lg p-10 overflow-hidden relative group">
+            <div className="w-full h-full flex justify-between">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-6xl font-bold text-neutral-900 dark:text-neutral-50">{title}</h1>
                     <p className="text-neutral-800 dark:text-neutral-50 text-2xl text-balance">{description}</p>
                 </div>
-                <div className="group w-fit h-fit">
+                <div className="group-[button]: w-fit h-fit z-10">
+                    <Link href="/projects/1">
                     <MoveDiagonal 
                         size={36} 
-                        className="text-neutral-900 dark:text-neutral-50 group-hover:text-neutral-700 dark:group-hover:text-neutral-300 transition-all duration-200 ease-in-out group-hover:scale-110" 
+                        className="text-neutral-900 dark:text-neutral-50 group-[button]:hover:text-neutral-700 dark:group-[button]:hover:text-neutral-300 transition-all duration-200 ease-in-out group-[button]:hover:scale-110"
                     />
+                    </Link>
                 </div>
             </div>
             <div 
-                className="w-full h-full flex justify-center items-center relative"
+                className="w-full h-full flex justify-center items-center relative z-10"
                 
             >
                 <Card 
-                    className={`absolute top-0 left-0 w-fit h-fit rotate-6 transition-all duration-300 ease-in-out shadow-lg hover:shadow-xl ${
-                        isHovered ? 'translate-x-4 -translate-y-4' : ''
-                    }`}
+                    className={`absolute top-0 left-0 w-fit h-fit rotate-6 transition-all duration-300 ease-in-out shadow-lg hover:shadow-xl group-hover:translate-x-4 group-hover:-translate-y-4 `}
                 >
                     <Image 
                         src="https://nicolas-becharat.com/_next/image?url=%2Fimages%2Fprojects%2Flearn404.png&w=828&q=75"
@@ -43,9 +42,7 @@ export default function CardProject() {
                     />
                 </Card>
                 <Card 
-                    className={`absolute top-0 -right-20 w-fit h-fit -rotate-3 transition-all duration-300 ease-in-out shadow-lg hover:shadow-xl ${
-                        isHovered ? '-translate-x-4 -translate-y-4' : ''
-                    }`}
+                    className={`absolute top-0 -right-20 w-fit h-fit -rotate-3 transition-all duration-300 ease-in-out shadow-lg hover:shadow-xl group-hover:-translate-x-4 group-hover:-translate-y-4`}
                 >
                     <Image 
                         src="https://nicolas-becharat.com/_next/image?url=%2Fimages%2Fprojects%2Flearn404.png&w=828&q=75"
@@ -56,6 +53,8 @@ export default function CardProject() {
                     />
                 </Card>
             </div>
+            <div className="absolute bottom-auto left-auto right-10 top-10 h-[500px] w-[500px] translate-y-[20%] rounded-full bg-[rgba(99,102,241,0.5)]  blur-[80px] opacity-0 group-hover:opacity-50 transition-all duration-300 ease-in-out"></div>
+
         </Card>
     )
 }
