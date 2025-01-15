95f32df76aa2ff4eae75b498e8ebe174c6818436
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-15 21:35:20 +0100

    chore: skeleton components

--- components/skeleton/time.tsx
+++ components/skeleton/time.tsx
@@ -1,12 +1,14 @@
+import { Skeleton } from "../ui/skeleton"
+
 export default function TimeSkeleton() {
 
     return (
         <div className="flex flex-col justify-center gap-2">
             <div className="flex items-center space-x-2">
-                <span className="w-24 h-4 bg-neutral-200 animate-pulse rounded"></span>
+                <Skeleton className="w-24 h-4" />
             </div>
             <div className="flex flex-col space-y-2">
-                <span className="w-20 h-4 bg-neutral-200 animate-pulse rounded"></span>
+            <Skeleton className="w-20 h-4" />
             </div>
         </div>
     )
