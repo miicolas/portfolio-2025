7b3252c28ca283912e7950b467d01e3cb2d1bf6a
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-17 19:22:29 +0100

    chore: session expire / update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/index";
- 
+
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql",
     }),
-    socialProviders: { 
-        github: { 
-         clientId: process.env.GITHUB_CLIENT_ID!, 
-         clientSecret: process.env.GITHUB_CLIENT_SECRET!, 
-        } 
-     }, 
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        }
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7,
+        updateAge: 60 * 60 * 24 
+    }
 });
