21fe27ad0fe0a71ebb78d74eec080fe2cd584b5f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-17 19:22:48 +0100

    style: spacing skills section

--- components/layout/skills.tsx
+++ components/layout/skills.tsx
@@ -8,8 +8,8 @@ import { useGetSkillsStore } from "@/store/get-skills"
 
 export const Skill = ({ className, logo, name, style }: { className: string, logo: string, name: string, style: React.CSSProperties }) => {
     return (
-        <Card className={`p-5 rounded-lg bg-neutral-100 dark:bg-neutral-900 absolute w-fit h-fit ${className}`} style={style}>
-            <Image src={logo} alt={name} width={1200} height={1200} className="w-12 h-12 m-auto" />
+        <Card className={`p-2 rounded-lg bg-neutral-100 dark:bg-neutral-900 absolute w-fit h-fit ${className} w-16 h-16`} style={style}>
+            <Image src={logo} alt={name} width={1200} height={1200} className=" m-auto max-w-full max-h-full object-contain" />
         </Card>
     )
 }
@@ -38,11 +38,10 @@ export default function Skills() {
     ][index % 2];
 
     return (
-        <>
-            <div className="py-16 mt-16" id="skills">
-                <Badge name="Skills" description="Skills is the reflect of my developer life and my passion for the web. I have worked on many projects and I am proud of the results." />
-            </div>
-            <div className="relative flex justify-center items-center p-32">
+        <div className="py-16" id="skills">
+
+            <Badge name="Skills" description="Skills is the reflect of my developer life and my passion for the web. I have worked on many projects and I am proud of the results." />
+            <div className="relative flex justify-center items-center py-64">
                 <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-balance text-5xl leading-16">
                     I work with the <span className="font-light italic text-indigo-500 mr-2">most powerful</span> technologies to build the best products.
                 </p>
@@ -60,6 +59,6 @@ export default function Skills() {
                 ))}
 
             </div>
-        </>
+        </div>
     );
 }
