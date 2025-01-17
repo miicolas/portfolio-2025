9fd0f825d858386549bfec8f817253fc9ede6b38
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-17 11:40:22 +0100

    build: footer social media

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,30 @@
+'use client'
+
+import Link from "next/link";
+import { motion } from "motion/react";
+import Logo from "../ui/logo";
+
 export default function Footer() {
+
+    const socialMedia = () => [
+        { name: 'Github', href: 'https://github.com/nicolasbecharat', icon: 'github' },
+        { name: 'LinkedIn', href: 'https://www.linkedin.com/in/nicolas-becharat/', icon: 'linkedin' },
+
+    ]
+
     return (
-        <div className="bg-neutral-50 dark:bg-neutral-900 w-full h-fit pt-16">
-            <div className="flex justify-center items-center p-32">
-                <p className="text-center text-balance text-2xl font-medium leading-5">Footer</p>
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-16 opacity-15 blur-[50px] bg-indigo-500 rounded-full"></div>
+        <div className="bg-neutral-50 dark:bg-neutral-900 w-full h-fit pt-16 pb-8">
+            <div className="flex justify-between items-center">
+                <p className="text-center text-balance text-2xl font-light leading-5">@nicolas-becharat</p>
+                <div className="flex gap-4 items-end">
+                    {socialMedia().map(({ name, href, icon }, index) => (
+                        <Link key={name} href={href}>
+                            <motion.div initial={{ rotate: index % 2 === 0 ? 3 : -3 }} whileHover={{ rotate: index % 2 === 0 ? 8 : -8, translateY: -20 }} whileTap={{ scale: 0.9 }} className="p-4 rounded-lg bg-neutral-200 flex items-center justify-center cursor-pointer origin-bottom-left shadow-lg">
+                                <Logo src={`/${icon}.svg`} alt={name} className="w-8 h-8" width={256} height={256} />
+                            </motion.div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { LogoData } from "@/lib/types";
+import { cn } from "@/lib/utils";
 
-export default function Logo({src, alt, width, height} : LogoData) {
+export default function Logo({src, alt, width, height, className} : LogoData) {
     return(
-        <Image src={src} alt={alt} width={width} height={height} className="rounded-lg" />
+        <Image src={src} alt={alt} width={width} height={height} className={cn(`${className}`)} />
     )
 }
