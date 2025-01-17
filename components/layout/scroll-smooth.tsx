fc722526d43af067a1f332e7b4661c0a4ec0b158
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-17 11:43:59 +0100

    chore: props components

--- components/layout/scroll-smooth.tsx
+++ components/layout/scroll-smooth.tsx
@@ -3,10 +3,10 @@
 import { ReactLenis } from 'lenis/react';
 import { ReactNode } from 'react';
 
-export default function ScrollSmooth({ children }: { children: ReactNode }) {
+export default function ScrollSmooth({ children, duration }: { children: ReactNode, duration: number }) {
 
   return (
-    <ReactLenis root options={{ duration: 1.2, smoothWheel: true }}>
+    <ReactLenis root options={{ duration: duration, smoothWheel: true }}>
     {children}
   </ReactLenis>
   )
