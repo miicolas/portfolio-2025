a3af6bd29e6cd2741ed45778ca553363890d9b27
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-24 11:26:17 +0100

    build: multi select filter

--- app/(client)/projects/page.tsx
+++ app/(client)/projects/page.tsx
@@ -1,9 +1,34 @@
+"use client";
 
+import React, { useState } from "react";
+import { MultiSelect } from "@/components/layout/multi-select";
+import { Cat, Dog, Fish, Rabbit, Turtle } from "lucide-react";
 
+const frameworksList = [
+  { value: "react", label: "React", icon: Turtle },
+  { value: "angular", label: "Angular", icon: Cat },
+  { value: "vue", label: "Vue", icon: Dog },
+  { value: "svelte", label: "Svelte", icon: Rabbit },
+  { value: "ember", label: "Ember", icon: Fish },
+];
 export default function Projects() {
+
+  const [selectedFrameworks, setSelectedFrameworks] = useState<string[]>(["react", "angular"]);
+
+
   return (
     <>
       <h1>Projects</h1>
+      <MultiSelect
+        options={frameworksList}
+        onValueChange={setSelectedFrameworks}
+        defaultValue={selectedFrameworks}
+        placeholder="Select frameworks"
+        variant="inverted"
+        animation={2}
+        maxCount={5}
+      />
+
     </>
   );
 }
