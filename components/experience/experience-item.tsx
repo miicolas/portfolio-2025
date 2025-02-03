93ba5259118e89d819fd6b28de4ade7f815fd842
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-03 21:29:42 +0100

    fix: type error any

--- components/experience/experience-item.tsx
+++ components/experience/experience-item.tsx
@@ -1,7 +1,5 @@
 import { motion } from "motion/react";
-import { ExternalLink } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import { Separator } from "../ui/separator";
 import { formatISODate } from "@/lib/utils";
 
