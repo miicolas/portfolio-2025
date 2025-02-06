077186b534be5d6c5603c0abbcf9a2eec2d869b2
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-06 20:19:12 +0100

    feat: ui style buttons

--- app/(client)/contact/page.tsx
+++ app/(client)/contact/page.tsx
@@ -10,7 +10,16 @@ export default function Contact() {
 
             <Badge name="Contact" description="Contact me to get in touch with me." />
             <motion.div className="border border-neutral-200 rounded-lg px-8 py-4 max-w-xl mx-auto shadow-md" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                <h1>Compose email</h1>
+
+                <div className="flex items-center justify-between">
+                    <h1>Compose email</h1>
+                    <div className="flex space-x-2">
+                        <div className="h-4 w-4 rounded-full bg-green-600 border border-neutral-300"></div>
+                        <div className="h-4 w-4 rounded-full bg-orange-600 border border-neutral-300"></div>
+                        <div className="h-4 w-4 rounded-full bg-red-600 border border-neutral-300"></div>
+                    </div>
+                </div>
+
                 <ContactForm />
             </motion.div>
 
